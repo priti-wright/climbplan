c2c435b9714df651f04a825aa7c8b01095d63423
thatneat <jrussellcurtis@gmail.com>
2015-09-19 13:05:56 -0700

    used actual Google API for lat/lng

--- app/store/SearchedPlaceStore.js
+++ app/store/SearchedPlaceStore.js
@@ -20,8 +20,8 @@ var SearchedPlaceStore = {
     updatePlace:(newPlace)=>{
         // Yeah sure this is supposed to go through an event dispatcher. maybe eventually
         var name = newPlace.name.replace('Mt ', 'Mount ') // Google likes to abbreviate this :(
-        var lat = newPlace.geometry.location.G
-        var lon = newPlace.geometry.location.K
+        var lat = newPlace.geometry.location.lat();
+        var lon = newPlace.geometry.location.lng();
         place = _.merge(
             {
                 lat,
