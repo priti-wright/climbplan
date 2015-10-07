10570d3b1a0fdcceafd1b8c7ee84f85f8630f6de
thatneat <jrussellcurtis@gmail.com>
2015-10-06 21:24:18 -0700

    group links helpfully

--- app/components/ResultPanel.jsx
+++ app/components/ResultPanel.jsx
@@ -14,7 +14,23 @@ var TargetSummary = React.createClass({
         var place = this.props.place
         return <div>
             Target: <strong>{place.name}</strong>, <i>{place.types[0].replace('_', ' ')} </i> 
-            located at ({place.lat}, {place.lon})
+            located at ({place.latShort}, {place.lonShort})
+        </div>
+    }
+})
+
+
+const ResearchLinkGroup = React.createClass({
+    propTypes: {
+        title: React.PropTypes.string.isRequired,
+        children: React.PropTypes.node.isRequired,
+    },
+    render(){
+        return <div className="research-link-group">
+            <h3>{this.props.title}</h3>
+            <ul>
+                {this.props.children}
+            </ul>
         </div>
     }
 })
@@ -26,16 +42,22 @@ var ResearchLink = React.createClass({
     },
     render(){
         const onClick = () => trackOutboundLink(this.props.url);
-        return <a 
+        return <li>
+            <a 
                 href={this.props.url}
                 onClick={onClick}
                 target="_blank"
                 className="site"
             >
-            {this.props.siteName}
-        </a>
+                {this.props.siteName}
+            </a>
+        </li>
     }
 })
+
+const betaTrsCategory = 'Beta & Trip Reports';
+const mapsCategory = 'Maps';
+const weatherCategory = 'Weather';
 
 var ResearchSuggestions = React.createClass({
     propTypes: placeProp,
@@ -45,42 +67,61 @@ var ResearchSuggestions = React.createClass({
         var links = [
           {
             site: 'Peakbagger',
+            category: betaTrsCategory,
             url: get_peakbagger_link(place)
           },
           {
             site: 'SummitPost',
+            category: betaTrsCategory,
             url: `http://www.summitpost.org/object_list.php?object_type=1&object_name_1=${place.namePlussed}`
           },
           {
             site: 'WTA',
+            category: betaTrsCategory,
             url: `http://www.wta.org/go-hiking/trip-reports/tripreport_search?title=${place.namePlussed}`
           },
           {
             site: 'Weather.gov',
+            category: weatherCategory,
             url: `http://forecast.weather.gov/MapClick.php?lon=${place.lon}&lat=${place.lat}`
           },
           {
             site: 'Mountain-Forecast',
+            category: weatherCategory,
             url: `http://www.mountain-forecast.com/peaks/${place.nameHypenated}`
           },
           {
             site: 'CalTopo',
+            category: mapsCategory,
             url: `http://caltopo.com/map.html#ll=${place.lat},${place.lon}&z=14&b=t`
           },
           {
             site: 'Cascade Climbers',
+            category: betaTrsCategory,
             url: `http://cascadeclimbers.com/forum/ubbthreads.php/ubb/tripreportsbeta`,
             integrated: false
           },
         ];
+        const groups = _.groupBy(
+            links,
+            'category'
+        );
+        const groupElements = _.map(
+            groups,
+            function(groupLinks, groupName){
+                return <ResearchLinkGroup title={groupName} key={groupName}>
+                    {_.map(
+                        groupLinks,
+                        function(link){
+                            return <ResearchLink siteName={link.site} url={link.url} key={link.site + link.url}/>;
+                        }
+                    )}
+                </ResearchLinkGroup>;
+            }
+        )
 
         return <div className="research-suggestions">
-        {_.map(
-            links,
-            function(link){
-                return <ResearchLink siteName={link.site} url={link.url} key={link.site + link.url}/>;
-            }
-        )}
+            {groupElements}
         </div>
     }
 })
