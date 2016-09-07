b35273b1888baa65e0a4b44721a977e963894034
thatneat <jrussellcurtis@gmail.com>
2016-09-07 09:01:38 -0700

    more eslint --fix

--- app/actions/searchMatches.js
+++ app/actions/searchMatches.js
@@ -4,25 +4,25 @@ export const REQUEST_SEARCH_MATCHES = 'REQUEST_SEARCH_MATCHES';
 export const RECEIVE_SEARCH_MATCHES = 'RECEIVE_SEARCH_MATCHES';
 
 export function requestSearchMatches(query) {
-   return {
-       type: REQUEST_SEARCH_MATCHES,
-       query,
-   };
+    return {
+        type: REQUEST_SEARCH_MATCHES,
+        query,
+    };
 }
 
 export function receiveSearchMatches(searchMatches) {
-   return {
-       type: RECEIVE_SEARCH_MATCHES,
-       searchMatches,
-   };
+    return {
+        type: RECEIVE_SEARCH_MATCHES,
+        searchMatches,
+    };
 }
 
 
 export function fetchSearchMatches(query) {
-   return (dispatch) => {
-       dispatch(requestSearchMatches(query));
+    return dispatch => {
+        dispatch(requestSearchMatches(query));
 
-       searchGeonamesPlaces(query)
+        searchGeonamesPlaces(query)
            .then(searchMatches => dispatch(receiveSearchMatches(searchMatches)));
-   };
+    };
 }
--- app/ga.js
+++ app/ga.js
@@ -1,18 +1,18 @@
 
-export function initGA(){
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+export function initGA() {
+    (function(i, s, o, g, r, a, m) {i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function() {
+        (i[r].q = i[r].q || []).push(arguments);}, i[r].l = 1 * new Date(); a = s.createElement(o),
+    m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m);
+    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
     ga('create', 'UA-64580382-1', 'auto');
     ga('send', 'pageview');
 }
 
-export function trackOutboundLink(link){
-  ga('send', 'event', 'outbound', 'click', link);
+export function trackOutboundLink(link) {
+    ga('send', 'event', 'outbound', 'click', link);
 }
 
-export function trackSearchComplete(place){
-  ga('send', 'event', 'search', 'completed', place.place_id);
+export function trackSearchComplete(place) {
+    ga('send', 'event', 'search', 'completed', place.place_id);
 }
