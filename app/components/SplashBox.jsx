ddf287c326bafc199cd07841a797467aa36448ca
thatneat <jrussellcurtis@gmail.com>
2016-09-08 22:38:04 -0700

    don't need this API key

--- app/components/SplashBox.jsx
+++ app/components/SplashBox.jsx
@@ -4,8 +4,6 @@ import SearchBox from '../containers/SearchBox';
 import ResultBox from './ResultBox.jsx';
 import styles from './SplashBox.scss';
 
-const googleStaticMapsApiKey = 'AIzaSyAouSFTFEITDfEGYoGaU5Nhr8hQvk7c_-0';
-
 
 const SplashBox = props => <ResultBox>
     <title>ClimbPlan</title>
