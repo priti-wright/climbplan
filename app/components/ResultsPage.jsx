08ad78ba755ae1374bbf29ec46b43d33b8e1af44
thatneat <jrussellcurtis@gmail.com>
2016-09-08 20:26:00 -0700

    add splashbox component

--- app/components/ResultsPage.jsx
+++ app/components/ResultsPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import SearchBox from '../containers/SearchBox';
 import SearchMap from '../containers/SearchMap';
 import ResultPanel from '../containers/ResultPanel';
 
 const ResultsPage = () => (
     <div>
       <SearchMap />
+      <SearchBox />
       <ResultPanel />
     </div>
 );
